952b04e406b6f1a4e8818c5b6331b2c483090afa
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-12-03 16:01:15 +0530

    Gstreamer tutorial-1 for streaming implemented as a test

--- tests/test.c
+++ tests/test.c
@@ -1,3 +1,9 @@
+#include "glib.h"
+#include "gst/gstbus.h"
+#include "gst/gstclock.h"
+#include "gst/gstelement.h"
+#include "gst/gstmessage.h"
+#include "gst/gstparse.h"
 #include <stdio.h>
 #include <gstreamer-1.0/gst/gst.h>
 
@@ -5,4 +11,43 @@
 #include <TargetConditionals.h>
 #endif
 
-int tutorial_main(int argc, char *argv[]);
+int tutorial_main(int argc, char *argv[]) {
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
+
+    gst_init(&argc, &argv);
+
+    pipeline = gst_parse_launch("playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
+            NULL);
+
+    // Start playing
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    // will block till error or EOS (End of stream)
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+
+    if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
+        g_printerr("An error occurred! Re-run with the GST_DEBUG=*:WARN "
+                "environment variable set for more details.\n");
+    }
+
+    // Freeing Resources
+    gst_message_unref (msg);
+    gst_object_unref (bus);
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    gst_object_unref (pipeline);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    printf("Hello World\n");
+
+#if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
+    return get_macos_main((GstMainFunc)tutorial_main, argc, argv, NULL);
+#else
+    return tutorial_main(argc, argv);
+#endif
+}
