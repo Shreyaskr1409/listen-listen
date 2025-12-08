9fdb3649cfc996fa323204f870419273e27059d7
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-12-09 00:13:09 +0530

    Dynamic pipeline to handle raw audio from a raw source created and it works nicely

--- tests/test.c
+++ tests/test.c
@@ -65,7 +65,7 @@ int tutorial_main(int argc, char *argv[]) {
     g_signal_connect(data.source, "pad-added", G_CALLBACK(pad_added_handler), &data);
 
     ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
-    if (ret == GST_STATE_PLAYING) {
+    if (ret == GST_STATE_CHANGE_FAILURE) {
         g_printerr("Unable to set pipeline to a playing state.\n");
         gst_object_unref(data.pipeline);
         return -1;
@@ -95,24 +95,69 @@ int tutorial_main(int argc, char *argv[]) {
                 case GST_MESSAGE_STATE_CHANGED:
                     if (GST_MESSAGE_SRC(msg) == GST_OBJECT(data.pipeline)) {
                         GstState old_state, new_state, pending_state;
-                        gst_message_parse_state_changed(msg, &old_state, &new_state, *pending_state);
-                        g_print("State changed from %s to %s:\n",
-                                gst_state_change_get_name(old_state), gst_state_change_get_name(new_state));
+                        gst_message_parse_state_changed(msg, &old_state, &new_state, &pending_state);
+                        g_print("State changed from %s to %s.\n",
+                                gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
                     }
                     break;
                 default:
                     g_printerr("Unexpected message received.\n");
                     break;
             }
-            gst_object_unref(msg);
+            // this is message_unref, not object unref. REMEMBER!!!
+            gst_message_unref(msg);
         }
     } while (!terminate);
 
-    gst_object_unref (bus);
+    gst_object_unref(bus);
     gst_element_set_state (data.pipeline, GST_STATE_NULL);
-    gst_object_unref (data.pipeline);
+    gst_object_unref(data.pipeline);
 
     return 0;
+}
+
+static void pad_added_handler (GstElement * src, GstPad * new_pad, CustomData * data) {
+    GstPad *sink_pad = gst_element_get_static_pad (data->convert, "sink");
+    GstPadLinkReturn ret;
+    GstCaps *new_pad_caps = NULL;
+    GstStructure *new_pad_struct = NULL;
+    const gchar *new_pad_type = NULL;
+
+    g_print("Received new pad '%s' from '%s':\n", GST_PAD_NAME (new_pad),
+            GST_ELEMENT_NAME (src));
+
+    // If our converter is already linked, we have nothing to do here
+    if (gst_pad_is_linked(sink_pad)) {
+        g_print ("We are already linked. Ignoring.\n");
+        goto exit;
+    }
+
+    // Check the new pad's type
+    new_pad_caps = gst_pad_get_current_caps(new_pad);
+    new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
+    new_pad_type = gst_structure_get_name(new_pad_struct);
+
+    if (!g_str_has_prefix(new_pad_type, "audio/x-raw")) {
+        g_print ("It has type '%s' which is not raw audio. Ignoring.\n",
+                new_pad_type);
+        goto exit;
+    }
+
+    // Attempt the link
+    ret = gst_pad_link(new_pad, sink_pad);
+    if (GST_PAD_LINK_FAILED(ret)) {
+        g_print ("Type is '%s' but link failed.\n", new_pad_type);
+    } else {
+        g_print ("Link succeeded (type '%s').\n", new_pad_type);
+    }
+
+exit:
+    // Unreference the new pad's caps, if we got them
+    if (new_pad_caps != NULL)
+        gst_caps_unref(new_pad_caps);
+
+    // Unreference the sink pad
+    gst_object_unref(sink_pad);
 }
 
 int main (int argc, char *argv[]) {
